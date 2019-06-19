62dda21b57f063f512b665a92d38fbdab3e922f4
Anuj Mushannavar <anujumushannavar@gmail.com>
2019-06-19 17:56:30 +0530

    Update arrayFrequencyCounter.js

--- arrayFrequencyCounter.js
+++ arrayFrequencyCounter.js
@@ -1,13 +1,32 @@
 function squaredItems(arr1, arr2){
-    if (arr1.length !== arr2.length) return false;
-    for(let i=0; i<arr1.length; i++){
-        let index = arr2.indexOf(arr1[i]**2);
-        if(index === -1){
-            return false;
+    if(arr1.length !== arr2.length) return false;
+
+    let frequencyCounter1 = {}
+
+    let frequencyCounter2 = {}
+
+    for(let val of arr1){
+        if(val in frequencyCounter1) {
+            frequencyCounter1[val]++;
+        }else{
+            frequencyCounter1[val] = 1;
         }
-        arr2.splice(index, 1);
-    });
+    }
+    for(let val of arr2){
+        if(val in frequencyCounter2) {
+            frequencyCounter2[val]++;
+        }else{
+            frequencyCounter2[val] = 1;
+        }
+    }
+    console.log(frequencyCounter1)
+    console.log(frequencyCounter2)
+
+    for (let key in frequencyCounter1){
+        if(!(key**2 in frequencyCounter2)) return false;
+        if(frequencyCounter2[key**2] !== frequencyCounter1[key]) return false;
+    }
     return true;
 }
 
-console.log(squaredItems([1,2,3],[6,4,1]))
+console.log(squaredItems([1,2,3,1],[9,4,1,1])) //O(n) time complexity
